a5c4737edb0a42d8ce90c357aefcd724e6763d7b
tssqzr <1214661466@qq.com>
2019-08-02 10:32:29 +0800

    更新邮箱状态码

--- task/EmailVerifyTask.cs
+++ task/EmailVerifyTask.cs
@@ -172,18 +172,21 @@ namespace NEL_FutureDao_BT.task
 
     class EmailState
     {
-        public const string sendBeforeState = "10100";
-        public const string sendAfterState = "10101";
-        public const string hasVerify = "10102";
-        public const string sendBeforeStateAtResetPassword = "10103";
-        public const string sendAfterStateAtResetPassword = "10104";
-        public const string hasVerifyAtResetPassword = "10105";
-        public const string sendBeforeStateAtChangeEmail = "10106";
-        public const string sendAfterStateAtChangeEmail = "10107";
-        public const string hasVerifyAtChangeEmail = "10108";
-        public const string sendBeforeStateAtInvited = "10109";
-        public const string sendAfterStateAtInvited = "10110";
-        public const string hasVerifyAtInvitedYes = "10111";
-        public const string hasVerifyAtInvitedNot = "10112";
+        public const string sendBeforeState = "10101";
+        public const string sendAfterState = "10102";
+        public const string hasVerify = "10103";
+        public const string hasVerifyFailed = "10104";
+        public const string sendBeforeStateAtResetPassword = "10105";
+        public const string sendAfterStateAtResetPassword = "10106";
+        public const string hasVerifyAtResetPassword = "10107";
+        public const string hasVerifyAtResetPasswordFailed = "10108";
+        public const string sendBeforeStateAtChangeEmail = "10109";
+        public const string sendAfterStateAtChangeEmail = "10110";
+        public const string hasVerifyAtChangeEmail = "10111";
+        public const string hasVerifyAtChangeEmailFailed = "10112";
+        public const string sendBeforeStateAtInvited = "10113";
+        public const string sendAfterStateAtInvited = "10114";
+        public const string hasVerifyAtInvitedYes = "10115";
+        public const string hasVerifyAtInvitedNot = "10116";
     }
 }
